038ba257599f98ff351dfa6df9dca05f236e19a2
Melanie <melywong@ucsc.edu>
2020-08-09 08:59:56 -0700

    messaging using socketio with flask

--- api/app.py
+++ api/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, json, request
+from flask_cors import CORS
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SECRET_KEY'] = 'secret!'
+socketIo = SocketIO(app, cors_allowed_origins="*")
+
+app.debug = True
+app.host = '0.0.0.0' # localhost?
+
+@socketIo.on("message")
+def handleMessage(msg):
+  print(msg)
+  send(msg, broadcast=True)
+  return None
 
 @app.route('/')
 def index():
     return 'hello world'
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8085, debug=True)
+  socketIo.run(app)
+  # app.run(host='0.0.0.0', port=8085, debug=True)
--- frontend/src/components/sharencare/ShareNCare.js
+++ frontend/src/components/sharencare/ShareNCare.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Title } from './Title';
 import { MessageList } from './MessageList';
 import { SendMessageForm } from './SendMessageForm';
+import io from "socket.io-client";
+
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
 
 const DUMMY_DATA = [
-    {
-      senderId: "perborgen",
-      text: "who'll win?"
-    },
-    {
-      senderId: "janedoe",
-      text: "who'll win?"
-    }
-  ]
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
 
-export class ShareNCare extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-           messages: DUMMY_DATA
-        }
-      }
+// Functional component
+export const ShareNCare = () => {
+  // create state using hooks
+  const [messages, setMessages] = useState(["Hello and Welcome"]);
+  const [message, setMessage] = useState("");
 
-    render() {
-        return(
-            <div>
-              {/* <Title /> */}
-              <MessageList messages={this.state.messages}/>
-              {/* <SendMessageForm /> */}
-            </div>
-        );
+  // componentDidUpdate method as hook (useEffect)
+  // this will auto call when message length changes
+  useEffect(() => {
+    getMessages();
+
+  }, [messages.length]);
+
+  // Called when first time app render and every time message length changes
+  const getMessages = () => {
+    socket.on("message", msg => {
+      setMessages([...messages, msg]);
+    });
+  };
+
+  const onChange = e => {
+    setMessage(e.target.value);
+  }
+
+  const onClick = e => {
+    if (message !== "") {
+      // When btn clicked emit the message to server
+      socket.emit("message", message);
+      setMessage("");
+    } else {
+      alert("Please Add A Message");
     }
-}
+  };
+
+  return (
+    <div>
+      {/* {messages.length > 0 && messages.map(msg => {
+        <div>
+          <p>{msg}</p>
+        </div>
+      })} */}
+      <input value={message} name="message" onChange={e => onChange(e)}/>
+      <button onClick={() => onClick()}>Send Message</button>
+    </div>
+  );
+};
+
+// export default ShareNCare;
+
+// export class ShareNCare extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//            messages: DUMMY_DATA // []
+//         }
+//       }
+
+//     render() {
+//         return(
+//             <div className="app">
+//               <Title />
+//               <MessageList messages={this.state.messages}/>
+//               <SendMessageForm />
+//             </div>
+//         );
+//     }
+// }
