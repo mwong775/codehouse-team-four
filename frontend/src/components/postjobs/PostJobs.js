09320c57a233dd28a658e04651dfa57156f5e3e5
Devika Shanbhag <devika16.ds@gmail.com>
2020-08-09 11:58:41 -0700

    Add placeholders for four options - search/add  jobs/talent

---
+++ frontend/src/components/postjobs/PostJobs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class PostJobs extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>PostJobs Component Works!</h1>
+            </div>
+        );
+    }
+}
