1596b50991a22e3d79d7e1ef23b8ac109c384a4d
Melanie <melywong@ucsc.edu>
2020-08-09 11:05:47 -0700

    add community channels

--- frontend/src/components/sharencare/ShareNCare.js
+++ frontend/src/components/sharencare/ShareNCare.js
@@ -4,6 +4,47 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './ShareNCare.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginRight: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
+
 let endPoint = "http://localhost:5000";
 let socket = io.connect(`${endPoint}`);
 
@@ -42,17 +83,51 @@ export const ShareNCare = () => {
     }
   };
 
+  const classes = useStyles();
+
   return (
-    <div className="chat">
-      {messages.map(msg => (
-        <div>
-          <p>{msg}</p>
+    <>
+      <main className="chat">
+        {messages.map(msg => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))}
+        <div className="input-form">
+          <TextField className="input" id="outlined-basic" label="Send A Message" variant="outlined" value={message} name="message" onChange={e => onChange(e)} />
+          <Button variant="contained" color="primary" onClick={() => onClick()}>Send</Button>
         </div>
-      ))}
-      <div className="input-form">
-        <TextField className="input" id="outlined-basic" label="Message" variant="outlined" value={message} name="message" onChange={e => onChange(e)} />
-        <Button variant="contained" color="primary" onClick={() => onClick()}>Send</Button>
-      </div>
-    </div>
+      </main>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="right"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <h2>Communities</h2>
+        <Divider />
+        <List>
+          {['General', 'Job Hunting','Remote Jobs'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Bored At Home', 'Quaranteens'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 };
