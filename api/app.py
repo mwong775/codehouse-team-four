1f925eca5c99117444bd120958334a5c8bcccc65
Melanie <melywong@ucsc.edu>
2020-08-09 22:30:19 -0700

    add tomtom api to flask backend

--- api/app.py
+++ api/app.py
@@ -2,6 +2,8 @@ from flask import Flask, json, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, send
 
+import requests
+
 app = Flask(__name__)
 CORS(app)
 
@@ -29,11 +31,27 @@ def handle_jobs():
     print(request.json)
     if request.method == 'GET':
       # respond with list of all jobs
-      return json.dumps({'message': 'Retrieved successfully', 'jobs': data }), 200
+      return json.dumps({'message': 'Retrieved successfully', 'jobs': data, 'count': len(data) }), 200
     elif request.method == 'POST':
       # add job to list of jobs
       data.append(request.json)
       return json.dumps({'message': 'Added successfully'}), 200
+
+@app.route('/mapsearch', methods=['GET'])
+def handle_mapsearch():
+  baseUrl = 'https://api.tomtom.com/search/2/categorySearch'
+  key = ''
+  # dummy inputs (not used)
+  lat = 37.763659
+  long = -122.485595
+  query = 'pizza'
+  limit = 5
+  radius = 10000
+
+  queryString = 'limit=5&lat=37.763659&lon=-122.485595&radius=10000&key=9L0w6Db1cMEBaDmsAi69ky2wIIkvAHPV'; #`limit=${limit}&lat=${lat}&lon=${long}&radius=${radius}&key=${this.state.apiKey}`
+  forward_url = baseUrl + '/pizza.json?' + queryString #`${baseUrl}/${query}.json?${queryString}`
+  response = requests.get(forward_url)
+  return response.content, response.status_code
 
 
 if __name__ == '__main__':
