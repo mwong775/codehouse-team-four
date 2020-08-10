22d607b23e4afba3eb3e2ac483bad6741d6d1f4d
Melanie <melywong@ucsc.edu>
2020-08-09 20:04:31 -0700

    GET and POST for jobssssssss

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ socketIo = SocketIO(app, cors_allowed_origins="*")
 app.debug = True
 app.host = 'localhost' # localhost?
 
-data = {}
+data = []
 
 @socketIo.on("message")
 def handleMessage(msg):
@@ -22,6 +22,19 @@ def handleMessage(msg):
 @app.route('/')
 def index():
     return json.dumps({'message': 'hello world'}), 200
+  
+@app.route('/jobs', methods=['GET', 'POST'])
+def handle_jobs():
+    # json should be None for GET request, not None for POST
+    print(request.json)
+    if request.method == 'GET':
+      # respond with list of all jobs
+      return json.dumps({'message': 'Retrieved successfully', 'jobs': data }), 200
+    elif request.method == 'POST':
+      # add job to list of jobs
+      data.append(request.json)
+      return json.dumps({'message': 'Added successfully'}), 200
+
 
 if __name__ == '__main__':
   socketIo.run(app, host='0.0.0.0')
