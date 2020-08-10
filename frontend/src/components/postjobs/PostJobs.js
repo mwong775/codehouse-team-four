f82e865d129957e5691ab9acf964cca30eeba701
Melanie <melywong@ucsc.edu>
2020-08-10 00:32:44 -0700

    merge conflict thing

--- frontend/src/components/postjobs/PostJobs.js
+++ frontend/src/components/postjobs/PostJobs.js
@@ -1,8 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import ReactDOM from 'react-dom';
->>>>>>> 33220c5184ef41a0c0647fb3690fc54ee7f80dd4
 import { Form, Field } from 'react-final-form';
 import { TextField, Checkbox, Radio, Select } from 'final-form-material-ui';
 import {
